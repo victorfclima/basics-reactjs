49189ff1d3e0f32c168878e5fb49575a16f43f17
Victor França <victorfclima@gmail.com>
2020-06-16 21:34:48 -0300

    Update: Added Add and Delete functions, tests ok

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from "react";
-
-import "./styles.css";
+import React from 'react';
+import api from './services/api';
+import './styles.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get('repositories').then(response => setRepositories(response.data));
+	}, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const response = await api.post('repositories', {
+			title: `Desafio ReactJS: ${Date.now()}`,
+			url: 'http://github.com/...',
+			techs: ['Node.js', '...'],
+		});
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+		const repository = response.data;
+		setRepositories([...repositories, repository]);
+	}
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+	async function handleRemoveRepository(id) {
+		await api.delete(`repositories/${id}`);
+
+		const otherRepositories = repositories.filter(
+			repository => repository.id != id
+		);
+
+		setRepositories(otherRepositories);
+	}
+
+	return (
+		<div>
+			<ul data-testid='repository-list'>
+				{repositories.map(repository => {
+					return (
+						<li key={repository.id}>
+							{repository.title}
+							<button onClick={() => handleRemoveRepository(repository.id)}>
+								Remover
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
